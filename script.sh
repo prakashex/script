04a9ee0b0c0d259bc3a1834ad6a1514c8b78b9dc
Prakash <prakashope@gmail.com>
2023-02-18 02:31:37 +0530

    added Browser router , Context

--- script.sh
+++ script.sh
@@ -41,12 +41,105 @@ sed '2d;6d;9d' temp | tee > temptwo
 sed -i '7i <div className="border-2 border-black text-center px-8 text-2xl">' temptwo
 sed -i '8i  Hi , there <br /> This React boiler plate code , with tailwind has been generated using a script  <br /> to change the contents of this file go to App.jsx inside src folder <br /> <br /> -- Prakash' temptwo
 
-rm temp App.jsx 
+rm temp App.jsx
 
 mv temptwo App.jsx
 
+mkdir Common\ Components
+mkdir Pages
+cd Pages
+
+echo "import React, { useContext } from 'react'
+import { Context } from '../Context/Context'
 
 
+function Home() {
+    const {testState} = useContext(Context)
+  return (
+    <div>Home
+        This variable is coming from Context -- {testState}
+    </div>
+
+  )
+}
+
+export default Home" > Home.jsx
+
+cd ..
+
+mkdir Context
+cd Context
+echo "import React , {createContext, useState} from '"react"'
+
+
+const Context = createContext()
+
+const {Provider} = Context
+
+
+function ContextProvider(props) {
+    const [testState , setTestState] = useState('test variable')
+
+
+    const {children} = props
+
+  return (
+    <Provider
+
+
+        value={{
+            testState
+        }}>
+
+        {children}
+
+    </Provider>
+  )
+}
+
+export {Context , ContextProvider}" > Context.jsx
+
+cd ..
+
+mkdir Config
+
+cd Config
+touch Config.jsx
+
+cd ..
+
+echo "import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
+import { ContextProvider } from './Context/Context'
+import './index.css'
+import Home from './Pages/Home'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/home',
+    element: <Home />
+  }
+])
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <React.StrictMode>
+    <ContextProvider>
+    <RouterProvider router={router} />
+    </ContextProvider>
+  </React.StrictMode>,
+)
+" > main.jsx
 
 cd ..
 
